62602904a613ddff21c1e722511f35f758cd294b
Juarez Rudsatz <juarez.rudsatz@ceabs.net>
2020-03-18 17:35:23 -0300

    add ctrl+c monitoring

---
+++ src/state.rs
@@ -0,0 +1,49 @@
+use ctrlc;
+use log::{debug, error};
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+// region Ctrl + C
+
+pub fn monitor_term_sinal() -> Arc<AtomicBool> {
+    lazy_static! {
+        static ref ABORTING: Arc<AtomicBool> = start_monitoring_term_sinal();
+    }
+    ABORTING.clone()
+}
+
+fn start_monitoring_term_sinal() -> Arc<AtomicBool> {
+    let handler = Arc::new(AtomicBool::new(false));
+    let aborting = handler.clone();
+
+    ctrlc::set_handler(move || {
+        error!("# Received Ctrl-C signal!!! Aborting...\n");
+
+        handler.store(true, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    debug!("# Waiting for Ctrl-C...");
+
+    aborting
+}
+
+// endregion
+
+// region trait Suggaring
+
+pub trait UserInterruption
+where
+    Self: Sized,
+{
+    fn aborted(&self) -> bool;
+}
+
+impl UserInterruption for Arc<AtomicBool> {
+    fn aborted(&self) -> bool {
+        self.load(Ordering::SeqCst)
+    }
+}
+
+// endregion
