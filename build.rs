3d743b3554194a4c7f15ce0c389685a4f8a2ecc2
Juarez Rudsatz <juarez.rudsatz@ceabs.net>
2020-04-03 03:12:27 -0300

    ci: gen completions in target/arch/release

--- build.rs
+++ build.rs
@@ -11,6 +11,7 @@ include!("src/args.rs");
 
 fn main() {
     println!("cargo:rerun-if-env-changed=PROFILE");
+    println!("cargo:rerun-if-env-changed=TARGET");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/args.rs");
 
@@ -20,13 +21,17 @@ fn main() {
 }
 
 pub(crate) fn build_artifacts() {
-    let pkg_name = std::env::var("CARGO_PKG_NAME").expect("Missing $CARGO_PKG_NAME!");
-    let pkg_dir = std::env::var("CARGO_MANIFEST_DIR").expect("Missing $CARGO_MANIFEST_DIR");
+    let pkg_dir = std::env::var("CARGO_MANIFEST_DIR").expect("# Missing $CARGO_MANIFEST_DIR!");
+    let target = std::env::var("TARGET").expect("# Missing $TARGET!");
 
-    let release = if cfg!(debug_assertions) { "debug" } else { "release" };
-    let out_dir = format!("{}/target/{}", pkg_dir, release);
-    eprintln!("# ARTIFACT_DIR: {}", out_dir);
+    let out_dir = if cfg!(debug_assertions) { 
+		format!("{}/target/debug", pkg_dir)
+	} else { 
+		format!("{}/target/{}/release", pkg_dir, target)
+	};
+    eprintln!("# out_dir: {}", out_dir);
 
+    let pkg_name = std::env::var("CARGO_PKG_NAME").expect("# Missing $CARGO_PKG_NAME!");
     let mut clap = Arguments::clap();
     clap.gen_completions(&pkg_name, clap::Shell::Bash, &out_dir);
     clap.gen_completions(&pkg_name, clap::Shell::Fish, &out_dir);
