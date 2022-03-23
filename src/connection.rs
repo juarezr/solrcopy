c03a2a970dd4113c405260e0eb4e5f9c3c16024b
Juarez Rudsatz <juarez.rudsatz.ter@estacio.br>
2022-03-23 18:21:27 -0300

    upgrade to ureq 2.4.0

--- src/connection.rs
+++ src/connection.rs
@@ -1,5 +1,6 @@
 use log::{debug, trace};
 use std::{error::Error, fmt};
+use std::time::Duration;
 
 use crate::helpers::*;
 
@@ -11,13 +12,8 @@ pub struct SolrError {
 }
 
 impl SolrError {
-    pub fn new(message: String, body: String) -> Self {
-        let msg = if body.is_empty() {
-            format!("Solr Error: {}", message)
-        } else {
-            format!("Solr Error: {} -> Reponse: {}", message, body)
-        };
-        SolrError { details: msg }
+    pub fn from(message: String) -> Self {
+        SolrError { details: message }
     }
 
     fn say(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -59,7 +55,12 @@ const SOLR_DEF_RETRIES: isize = 8;
 impl SolrClient {
     pub fn new() -> Self {
         let retries = env_value(SOLR_COPY_RETRIES, SOLR_DEF_RETRIES);
-        let client = ureq::agent()
+        let timeout = Self::get_timeout();
+        let duration = Duration::from_secs(timeout);
+
+        let builder = ureq::AgentBuilder::new();
+        let client = builder
+            .timeout(duration)
             // .basic_auth("admin", Some("good password"))
             .build();
 
@@ -69,23 +70,15 @@ impl SolrClient {
     fn get_timeout() -> u64 {
         let def = if cfg!(debug_assertions) { 6 } else { SOLR_DEF_TIMEOUT };
         let timeout: isize = env_value(SOLR_COPY_TIMEOUT, def);
-        timeout.to_u64() * 1000
-    }
-
-    fn set_timeout(builder: &mut ureq::Request) -> &mut ureq::Request {
-        let timeout = Self::get_timeout();
-        builder
-            .timeout_connect(timeout)
-            .timeout_read(Self::get_timeout())
-            .timeout_write(Self::get_timeout())
+        timeout.to_u64()
     }
 
     pub fn get_as_text(&mut self, url: &str) -> Result<String, SolrError> {
-        let mut builder = self.http.get(url);
-        let request = Self::set_timeout(&mut builder);
+        trace!("GET {}", url);
         loop {
-            let response = request.call();
-            let result = self.handle_response(response);
+            let request = self.http.get(url);
+            let answer = request.call();
+            let result = self.handle_response(answer);
             match result {
                 None => continue,
                 Some(retrieved) => break retrieved,
@@ -104,12 +97,12 @@ impl SolrClient {
     fn post_with_content_type(
         &mut self, url: &str, content_type: &str, content: &str,
     ) -> Result<String, SolrError> {
-        let mut builder = self.http.post(url);
-        let req = Self::set_timeout(&mut builder);
-        let request = req.set("Content-Type", content_type);
+        trace!("POST as {} {}", content_type, url);
         loop {
-            let response = request.send_string(content);
-            let result = self.handle_response(response);
+            let req = self.http.post(url);
+            let request = req.set("Content-Type", content_type);
+                let answer = request.send_string(content);
+            let result = self.handle_response(answer);
             match result {
                 None => continue,
                 Some(retrieved) => break retrieved,
@@ -117,125 +110,54 @@ impl SolrClient {
         }
     }
 
-    fn handle_response(&mut self, response: ureq::Response) -> Option<Result<String, SolrError>> {
-        let result = self.get_result_from(response);
+    fn handle_response(&mut self, answer: Result<ureq::Response, ureq::Error>) -> Option<Result<String, SolrError>> {
+        let result = self.decode_response(answer);
         match result {
-            Ok(retrieved) => {
+            Ok(content) => {
                 if self.retry_count > 0 {
                     self.retry_count -= 1;
                 }
-                Some(Ok(retrieved))
+                Some(Ok(content))
+            }
+            Err((fatal, failure)) => {
+                if fatal == false && self.retry_count < self.max_retries {
+                    self.retry_count += 1;
+                    debug!("Retry {}/{}: Response Error: {}", self.retry_count, self.max_retries, failure);
+                    // wait a little for the server recovering before retrying
+                    wait(5 * self.retry_count);
+                    None
+                } else {
+                    Some(Err(failure))
+                }
+            }
+        }
+    }
+
+    fn decode_response(&mut self, answer: Result<ureq::Response, ureq::Error>) -> Result<String, (bool, SolrError)> {
+        match answer {
+            Ok(content) => {
+                let body = content.into_string();
+                match body {
+                    Ok(content) =>Ok(content),
+                    Err(failed) => {
+                        Err((false, SolrError::from(failed.to_string())))
+                    }
+                }
             }
             Err(failure) => {
-                match self.handle_failure(failure) {
-                    None => {
-                        self.retry_count += 1;
-                        // wait a little for the server recovering before retrying
-                        wait(5 * self.retry_count);
-                        None
+                match failure {
+                    ureq::Error::Status(code, response) => {
+                        let fatal = code >= 500;
+                        let msg = response.status_text();
+                        Err((fatal, SolrError::from(msg.to_string())))
                     }
-                    Some(failed) => Some(Err(failed)),
-                }
-            }
-        }
-    }
-
-    fn get_result_from(
-        &mut self, response: ureq::Response,
-    ) -> Result<String, Result<ureq::Response, std::io::Error>> {
-        if response.error() {
-            Err(Ok(response))
-        } else {
-            match response.into_string() {
-                Ok(body) => Ok(body),
-                Err(read_error) => Err(Err(read_error)),
-            }
-        }
-    }
-
-    fn handle_failure(
-        &mut self, failure: Result<ureq::Response, std::io::Error>,
-    ) -> Option<SolrError> {
-        let can_retry = self.retry_count < self.max_retries;
-        match failure {
-            Ok(response) => {
-                if response.synthetic() {
-                    Self::handle_synthetic_error(can_retry, response)
-                } else {
-                    Self::handle_solr_error(can_retry, response)
-                }
-            }
-            Err(read_error) => Self::handle_receive_error(can_retry, read_error),
-        }
-    }
-
-    fn handle_synthetic_error(can_retry: bool, response: ureq::Response) -> Option<SolrError> {
-        let cause = response.synthetic_error().as_ref().unwrap();
-        match cause {
-            ureq::Error::ConnectionFailed(_) => {
-                Self::convert_synthetic_error(can_retry, cause, &response)
-            }
-            ureq::Error::Io(failure) => {
-                let error_kind = failure.kind();
-                match error_kind {
-                    std::io::ErrorKind::ConnectionRefused
-                    | std::io::ErrorKind::ConnectionReset
-                    | std::io::ErrorKind::ConnectionAborted
-                    | std::io::ErrorKind::NotConnected
-                    | std::io::ErrorKind::TimedOut
-                    | std::io::ErrorKind::Interrupted => {
-                        Self::convert_synthetic_error(can_retry, cause, &response)
+                    ureq::Error::Transport(transport) => {
+                        let cause = transport.message().unwrap_or("Network failure.");
+                        Err((false, SolrError::from(cause.to_string())))
                     }
-                    _ => Self::convert_synthetic_error(can_retry, cause, &response),
                 }
             }
-            _ => Self::convert_synthetic_error(false, cause, &response),
         }
-    }
-
-    fn convert_synthetic_error(
-        can_retry: bool, cause: &ureq::Error, response: &ureq::Response,
-    ) -> Option<SolrError> {
-        if can_retry {
-            debug!(
-                "Generic Error: Retry: {}, Status: {}",
-                cause.to_string(),
-                response.status_line()
-            );
-            return None;
-        }
-        let message = format!("Generic Error: {}", cause.status_text());
-        let body = cause.body_text();
-        trace!("Continue: {} -> {}", message, body);
-        Some(SolrError::new(message, body))
-    }
-
-    fn handle_solr_error(can_retry: bool, response: ureq::Response) -> Option<SolrError> {
-        let message = format!("Response Error: {}", response.status_line());
-        // Retry on status 502 Bad Gateway
-        // Retry on status 503 Service Temporarily Unavailable
-        // Retry on status 504 Gateway Timeout
-        if can_retry && response.server_error() {
-            debug!("Retry: {}", message);
-            return None;
-        }
-        let body = match response.into_string() {
-            Ok(content) => content,
-            Err(unread) => unread.to_string(),
-        };
-        trace!("Continue: {} -> {}", message, body);
-        Some(SolrError::new(message, body))
-    }
-
-    fn handle_receive_error(can_retry: bool, error: std::io::Error) -> Option<SolrError> {
-        let message = format!("Receive Error: {}", error.to_string());
-        let body = format!("{:?}", error);
-        if can_retry {
-            debug!("Retry: {} -> {}", message, body);
-            return None;
-        }
-        trace!("Continue: {} -> {}", message, body);
-        Some(SolrError::new(message, body))
     }
 
     // region Helpers
